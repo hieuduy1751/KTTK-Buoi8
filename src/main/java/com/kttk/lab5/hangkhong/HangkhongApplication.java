2c3761f78246ce3fca449ad842cf0ae206f69cd6
hieuduy1751 <hieuduy1751@gmail.com>
2022-10-31 09:42:32 +0700

    add: rest template

--- src/main/java/com/kttk/lab5/hangkhong/HangkhongApplication.java
+++ src/main/java/com/kttk/lab5/hangkhong/HangkhongApplication.java
@@ -7,6 +7,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableCircuitBreaker
 @EnableRetry
 public class HangkhongApplication {
+  @Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public RetryTemplate retryTemplate() {
+		RetryTemplate retryTemplate = new RetryTemplate();
+
+		FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+		fixedBackOffPolicy.setBackOffPeriod(500l);
+		retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+		SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+		retryPolicy.setMaxAttempts(2);
+		retryTemplate.setRetryPolicy(retryPolicy);
+		retryTemplate.setThrowLastExceptionOnExhausted(true);
+
+		return retryTemplate;
+	}
 
   public static void main(String[] args) {
     SpringApplication.run(HangkhongApplication.class, args);
